caa657a8a18d64b0441877cc2446f4150d67982d
Wolftrot_SamyIPelaez <samy.ibriz@eduvaud.ch>
2020-03-17 17:46:49 +0100

    re-exercice write-host bonjour

---
+++ 122-8-Script-Write-Host.ps1
@@ -0,0 +1,24 @@
+﻿<#
+.NOTES
+    *****************************************************************************
+	ETML
+	Name: 	 write-host.ps1
+    Author:	 Samy I.Pelaez
+    Date:	 17.03.2020
+ 	*****************************************************************************
+    Modifications
+ 	Date  : -
+ 	Author: -
+ 	Reason: -
+ 	*****************************************************************************
+.SYNOPSIS
+    Outputs "Bonjour"
+ 	
+.DESCRIPTION
+    Outputs "Bonjour" in to the console
+ 	
+.EXAMPLE
+    write-host.ps1 -string "Output this string"
+#>
+    
+write-host "Bonjour"
