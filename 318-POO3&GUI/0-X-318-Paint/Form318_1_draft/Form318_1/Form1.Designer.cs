9d38fd030fb047398707e58e41a77c2ffbd83ae1
wolftrot <samy.ibriz@eduvaud.ch>
2020-03-23 10:04:24 +0100

    318-Restructure

---
+++ 318-POO3&GUI/0-X-318-Paint/Form318_1_draft/Form318_1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Form318_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(232, 387);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(42, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(Form318_1.Form1);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(557, 274);
+            this.trackBar1.Maximum = 50;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 40);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 422);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 318-POO3&GUI/0-X-318-Paint/Form318_1_draft/Form318_1/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form318_1
+{
+    public partial class Form1 : Form
+    {
+        bool clicked;
+        int lastX, lastY;
+        //int endX, endY;
+        Graphics g;
+        Brush b;
+        int brushsize=1;
+        Pen p;
+        Point last;
+
+        List<Point> points = new List<Point>();
+
+        public int Brushsize
+        {
+            get
+            {
+                return brushsize;
+            }
+
+            set
+            {
+                brushsize = value;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            //g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.Invalid;
+            b = new SolidBrush(Color.Black);
+            p = new Pen(b, Brushsize);
+            //p.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+            //p.DashPattern=;
+
+
+            //p.DashCap = System.Drawing.Drawing2D.DashCap.Round;
+
+            // Create a custom dash pattern.
+            //p.DashPattern = new float[] { 40.0F, 20.0F, 10.0F, 30.0F };
+
+            //p.MiterLimit = p.Width * 1.25f;
+            //p.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+
+            // Draw a line.
+            //e.Graphics.DrawLine(greenPen, 20.0F, 20.0F, 100.0F, 240.0F);
+            //p.EndCap
+
+            this.trackBar1.DataBindings.Add("Value",
+                                this,
+                                "Brushsize",
+                                false,
+                                DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            showCoordinates(e);
+            if (clicked)
+            {
+                points.Add(e.Location);
+                //g.FillEllipse(b, e.X, e.Y, brushsize, brushsize);
+                //g.DrawEllipse()
+                //g.DrawLine(p, last,e.Location); 
+
+                if(Brushsize<=1)
+                    g.DrawLine(p, last, e.Location);
+                else
+                    g.DrawLines(p, points.ToArray());
+
+
+
+
+
+                lastX = e.X;
+                lastY = e.Y;
+                last = e.Location;
+                //this.Invalidate();
+            }
+                
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            showCoordinates(e);
+            //g.FillRectangle(b, e.X, e.Y, brushsize, brushsize);
+            points.Add(e.Location);
+            lastX = e.X;
+            lastY = e.Y;
+            last = e.Location;
+            clicked = true;
+            //g.DrawLine(p, 50, 50, 254, 254);
+        }
+
+ 
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            showCoordinates(e);
+            clicked = false;
+            /*if (points.Count % 2 != 0)
+            {
+                points.Add(e.Location);
+            }*/
+
+            
+            points.Clear();
+            //this.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.DrawLines(p, new Point[] { new Point(0, 50), new Point(500, 50) });
+            g.DrawLines(p, new Point[] { new Point(0, 100), new Point(400, 100) });
+        }
+
+
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = "  scroll value:" + trackBar1.Value + "   brushsize" + brushsize;
+            b = new SolidBrush(Color.Black);
+            p = new Pen(b, Brushsize);
+        }
+
+        private void showCoordinates(MouseEventArgs e)
+        {
+            label1.Text = "x:" + e.X + "   y:" + e.Y;
+        }
+        /*
+protected override void OnPaint(PaintEventArgs e)
+{
+   base.OnPaint(e);
+   for(int i=0;i<points.Count-1;i++)
+   {
+       g.DrawLine(p, points[i],points[i+1]);
+   }
+}*/
+    }
+}
