32846e52fb59658a472e1a088d040fb56c2f6d13
wolftrot <samy.ibriz@eduvaud.ch>
2020-03-30 17:20:02 +0200

    318-POO3-Paint-WIP

--- 318-POO3&GUI/0-X-318-Paint/Form318_1_draft/Form318_1/Form1.Designer.cs
+++ 318-POO3&GUI/0-X-318-Paint/Form318_1_draft/Form318_1/Form1.Designer.cs
@@ -31,69 +31,54 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label2 = new System.Windows.Forms.Label();
+            this.PointerCoordsLabel_display = new System.Windows.Forms.Label();
+            this.BrushSizeTrackBar = new System.Windows.Forms.TrackBar();
+            this.PointerCoordsLabel = new System.Windows.Forms.Label();
             this.penButton = new System.Windows.Forms.Button();
             this.fontainPenButton = new System.Windows.Forms.Button();
-            this.sizePanel = new System.Windows.Forms.Panel();
-            this.linePanel = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
+            this.sizeSamplePanel = new System.Windows.Forms.Panel();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.BrushSizeLabel = new System.Windows.Forms.Label();
+            this.drawingPanel = new System.Windows.Forms.Panel();
+            this.SelectedColorPanel = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
+            this.BrushSizeLabel_display = new System.Windows.Forms.Label();
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.BrushSizeTrackBar)).BeginInit();
+            this.sizeSamplePanel.SuspendLayout();
+            this.SelectedColorPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // PointerCoordsLabel_display
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(145, 620);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(14, 20);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "-";
+            this.PointerCoordsLabel_display.AutoSize = true;
+            this.PointerCoordsLabel_display.Location = new System.Drawing.Point(145, 620);
+            this.PointerCoordsLabel_display.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PointerCoordsLabel_display.Name = "PointerCoordsLabel_display";
+            this.PointerCoordsLabel_display.Size = new System.Drawing.Size(14, 20);
+            this.PointerCoordsLabel_display.TabIndex = 0;
+            this.PointerCoordsLabel_display.Text = "-";
             // 
-            // button1
+            // BrushSizeTrackBar
             // 
-            this.button1.Location = new System.Drawing.Point(15, 559);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(104, 56);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.BrushSizeTrackBar.Location = new System.Drawing.Point(16, 480);
+            this.BrushSizeTrackBar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.BrushSizeTrackBar.Maximum = 50;
+            this.BrushSizeTrackBar.Name = "BrushSizeTrackBar";
+            this.BrushSizeTrackBar.Size = new System.Drawing.Size(162, 69);
+            this.BrushSizeTrackBar.TabIndex = 2;
+            this.BrushSizeTrackBar.Scroll += new System.EventHandler(this.trackBar1_Scroll);
             // 
-            // form1BindingSource
+            // PointerCoordsLabel
             // 
-            this.form1BindingSource.DataSource = typeof(Form318_1.Form1);
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(15, 307);
-            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.trackBar1.Maximum = 50;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(162, 69);
-            this.trackBar1.TabIndex = 2;
-            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 620);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Pointer Location";
+            this.PointerCoordsLabel.AutoSize = true;
+            this.PointerCoordsLabel.Location = new System.Drawing.Point(13, 620);
+            this.PointerCoordsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PointerCoordsLabel.Name = "PointerCoordsLabel";
+            this.PointerCoordsLabel.Size = new System.Drawing.Size(124, 20);
+            this.PointerCoordsLabel.TabIndex = 3;
+            this.PointerCoordsLabel.Text = "Pointer Location";
             // 
             // penButton
             // 
@@ -108,125 +93,118 @@
             // 
             this.fontainPenButton.Location = new System.Drawing.Point(16, 61);
             this.fontainPenButton.Name = "fontainPenButton";
-            this.fontainPenButton.Size = new System.Drawing.Size(161, 40);
+            this.fontainPenButton.Size = new System.Drawing.Size(161, 45);
             this.fontainPenButton.TabIndex = 1;
             this.fontainPenButton.Text = "Fountain Pen";
             this.fontainPenButton.UseVisualStyleBackColor = true;
             // 
-            // sizePanel
+            // sizeSamplePanel
             // 
-            this.sizePanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.sizePanel.Location = new System.Drawing.Point(15, 112);
-            this.sizePanel.Name = "sizePanel";
-            this.sizePanel.Size = new System.Drawing.Size(50, 52);
-            this.sizePanel.TabIndex = 0;
-            // 
-            // linePanel
-            // 
-            this.linePanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.linePanel.Location = new System.Drawing.Point(71, 112);
-            this.linePanel.Name = "linePanel";
-            this.linePanel.Size = new System.Drawing.Size(50, 52);
-            this.linePanel.TabIndex = 1;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 282);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 20);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Brush Size";
+            this.sizeSamplePanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sizeSamplePanel.Controls.Add(this.panel1);
+            this.sizeSamplePanel.Location = new System.Drawing.Point(17, 112);
+            this.sizeSamplePanel.Name = "sizeSamplePanel";
+            this.sizeSamplePanel.Size = new System.Drawing.Size(70, 70);
+            this.sizeSamplePanel.TabIndex = 0;
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Location = new System.Drawing.Point(183, 15);
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Location = new System.Drawing.Point(82, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(875, 625);
-            this.panel1.TabIndex = 6;
+            this.panel1.Size = new System.Drawing.Size(80, 80);
+            this.panel1.TabIndex = 1;
             // 
-            // panel2
+            // BrushSizeLabel
             // 
-            this.panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel2.Location = new System.Drawing.Point(127, 112);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(50, 52);
-            this.panel2.TabIndex = 2;
+            this.BrushSizeLabel.AutoSize = true;
+            this.BrushSizeLabel.Location = new System.Drawing.Point(13, 455);
+            this.BrushSizeLabel.Name = "BrushSizeLabel";
+            this.BrushSizeLabel.Size = new System.Drawing.Size(86, 20);
+            this.BrushSizeLabel.TabIndex = 5;
+            this.BrushSizeLabel.Text = "Brush Size";
+            // 
+            // drawingPanel
+            // 
+            this.drawingPanel.BackColor = System.Drawing.Color.White;
+            this.drawingPanel.Location = new System.Drawing.Point(183, 15);
+            this.drawingPanel.Name = "drawingPanel";
+            this.drawingPanel.Size = new System.Drawing.Size(875, 600);
+            this.drawingPanel.TabIndex = 6;
+            // 
+            // SelectedColorPanel
+            // 
+            this.SelectedColorPanel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.SelectedColorPanel.Controls.Add(this.panel3);
+            this.SelectedColorPanel.Location = new System.Drawing.Point(107, 112);
+            this.SelectedColorPanel.Name = "SelectedColorPanel";
+            this.SelectedColorPanel.Size = new System.Drawing.Size(70, 70);
+            this.SelectedColorPanel.TabIndex = 2;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel3.Location = new System.Drawing.Point(15, 170);
+            this.panel3.Location = new System.Drawing.Point(82, 0);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(50, 52);
-            this.panel3.TabIndex = 3;
+            this.panel3.Size = new System.Drawing.Size(80, 80);
+            this.panel3.TabIndex = 1;
             // 
-            // panel4
+            // BrushSizeLabel_display
             // 
-            this.panel4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel4.Location = new System.Drawing.Point(71, 170);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(50, 52);
-            this.panel4.TabIndex = 2;
+            this.BrushSizeLabel_display.AutoSize = true;
+            this.BrushSizeLabel_display.Location = new System.Drawing.Point(145, 455);
+            this.BrushSizeLabel_display.Name = "BrushSizeLabel_display";
+            this.BrushSizeLabel_display.Size = new System.Drawing.Size(14, 20);
+            this.BrushSizeLabel_display.TabIndex = 9;
+            this.BrushSizeLabel_display.Text = "-";
+            this.BrushSizeLabel_display.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
-            // panel5
+            // form1BindingSource
             // 
-            this.panel5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.panel5.Location = new System.Drawing.Point(127, 170);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(50, 52);
-            this.panel5.TabIndex = 3;
+            this.form1BindingSource.DataSource = typeof(Form318_1.Form1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(1070, 649);
-            this.Controls.Add(this.panel5);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.BrushSizeLabel_display);
+            this.Controls.Add(this.SelectedColorPanel);
             this.Controls.Add(this.fontainPenButton);
             this.Controls.Add(this.penButton);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.linePanel);
-            this.Controls.Add(this.sizePanel);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.drawingPanel);
+            this.Controls.Add(this.BrushSizeLabel);
+            this.Controls.Add(this.sizeSamplePanel);
+            this.Controls.Add(this.PointerCoordsLabel);
+            this.Controls.Add(this.BrushSizeTrackBar);
+            this.Controls.Add(this.PointerCoordsLabel_display);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
-            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.BrushSizeTrackBar)).EndInit();
+            this.sizeSamplePanel.ResumeLayout(false);
+            this.SelectedColorPanel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label PointerCoordsLabel_display;
         private System.Windows.Forms.BindingSource form1BindingSource;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar BrushSizeTrackBar;
+        private System.Windows.Forms.Label PointerCoordsLabel;
         private System.Windows.Forms.Button penButton;
         private System.Windows.Forms.Button fontainPenButton;
-        private System.Windows.Forms.Panel sizePanel;
-        private System.Windows.Forms.Panel linePanel;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel sizeSamplePanel;
+        private System.Windows.Forms.Label BrushSizeLabel;
+        private System.Windows.Forms.Panel drawingPanel;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel SelectedColorPanel;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label BrushSizeLabel_display;
     }
 }
 
--- 318-POO3&GUI/0-X-318-Paint/Form318_1_draft/Form318_1/Form1.cs
+++ 318-POO3&GUI/0-X-318-Paint/Form318_1_draft/Form318_1/Form1.cs
@@ -17,53 +17,75 @@ namespace Form318_1
         bool clicked;
         int lastX, lastY;
         //int endX, endY;
-        Graphics g;
-        Graphics sizeSample;
-        Brush b;
-        private int brushsize = 1;
-        Pen p;
-        Point last;
+        Graphics DrawingZone;
+        Graphics SizeSample;
+        Graphics ColorSample;
+        Brush Brush;
+        private int BrushSize = 1;
+        private int BrushForm = 1;
+        Pen Pen;
+        Point LastPoint;
 
         List<Point> points = new List<Point>();
 
-        public int Brushsize {get{return brushsize;}set{brushsize = value;}}
+        public int Brushsize {get{return BrushSize;}set{BrushSize = value;}}
 
         public Form1()
         {
             InitializeComponent();
-            g = this.CreateGraphics();
-            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            //g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.Invalid;
-            b = new SolidBrush(Color.Black);
-            p = new Pen(b, Brushsize);
-            //p.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
-            //p.DashPattern=;
 
-            sizeSample = this.CreateGraphics();
-            sizeSample.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-            //p.DashCap = System.Drawing.Drawing2D.DashCap.Round;
+            //create graphic for the controller
+            DrawingZone = drawingPanel.CreateGraphics();
+            SizeSample = sizeSamplePanel.CreateGraphics();
+            ColorSample = SelectedColorPanel.CreateGraphics();
+
+            //graphics events
+            drawingPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            drawingPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            drawingPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+
+            //anti-aliasing (lissage)
+            var antiAlising = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            DrawingZone.SmoothingMode = antiAlising;
+            SizeSample.SmoothingMode = antiAlising;
+            ColorSample.SmoothingMode = antiAlising;
+
+            //quality of the composition
+            var compositionQuality = System.Drawing.Drawing2D.CompositingQuality.Invalid;
+            DrawingZone.CompositingQuality = compositionQuality;
+            SizeSample.CompositingQuality = compositionQuality;
+            ColorSample.CompositingQuality = compositionQuality;
+
+
+            //Tools
+            Brush = new SolidBrush(Color.Black);
+            Pen = new Pen(Brush, Brushsize);
+
+            //line shape
+            Pen.DashCap = System.Drawing.Drawing2D.DashCap.Round;
+
+            //line joints
+            Pen.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+
+            //line joint width
+            Pen.MiterLimit = Pen.Width * 1.25f;
 
             // Create a custom dash pattern.
-            //p.DashPattern = new float[] { 40.0F, 20.0F, 10.0F, 30.0F };
-
-            //p.MiterLimit = p.Width * 1.25f;
-            //p.LineJoin = System.Drawing.Drawing2D.LineJoin.Round;
+            // Pen.DashPattern = new float[] { 40.0F, 20.0F, 10.0F, 30.0F };
 
             // Draw a line.
-            //e.Graphics.DrawLine(greenPen, 20.0F, 20.0F, 100.0F, 240.0F);
-            //p.EndCap
+            // DrawingZone.DrawLine(Pen, 20.0F, 20.0F, 100.0F, 240.0F);
 
-            this.trackBar1.DataBindings.Add("Value",
+            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            this.BrushSizeTrackBar.DataBindings.Add(
+                                "Value",
                                 this,
                                 "Brushsize",
                                 false,
                                 DataSourceUpdateMode.OnPropertyChanged);
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            
-        }
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
@@ -76,13 +98,13 @@ namespace Form318_1
                 //g.DrawLine(p, last,e.Location); 
 
                 if(Brushsize<=1)
-                    g.DrawLine(p, last, e.Location);
+                    DrawingZone.DrawLine(Pen, LastPoint, e.Location);
                 else
-                    g.DrawLines(p, points.ToArray());
+                    DrawingZone.DrawLines(Pen, points.ToArray());
 
                 lastX = e.X;
                 lastY = e.Y;
-                last = e.Location;
+                LastPoint = e.Location;
                 //this.Invalidate();
             }
                 
@@ -91,12 +113,13 @@ namespace Form318_1
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
             showCoordinates(e);
-            //g.FillRectangle(b, e.X, e.Y, brushsize, brushsize);
+            //DrawingZone.FillRectangle(Brush, e.X, e.Y, BrushSize, BrushSize);
             points.Add(e.Location);
             lastX = e.X;
             lastY = e.Y;
-            last = e.Location;
+            LastPoint = e.Location;
             clicked = true;
+            DrawingZone.DrawLine(Pen, LastPoint, e.Location);
             //g.DrawLine(p, 50, 50, 254, 254);
         }
 
@@ -123,9 +146,9 @@ namespace Form318_1
         /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
-            g.Clear(Color.White);
-            g.DrawLines(p, new Point[] { new Point(0, 50), new Point(500, 50) });
-            g.DrawLines(p, new Point[] { new Point(0, 100), new Point(400, 100) });
+            DrawingZone.Clear(Color.White);
+            DrawingZone.DrawLines(Pen, new Point[] { new Point(0, 50), new Point(500, 50) });
+            DrawingZone.DrawLines(Pen, new Point[] { new Point(0, 100), new Point(400, 100) });
             
         }
 
@@ -133,23 +156,25 @@ namespace Form318_1
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            label2.Text = "  scroll value:" + trackBar1.Value + "   brushsize" + brushsize;
-            b = new SolidBrush(Color.Black);
-            p = new Pen(b, Brushsize);
+            BrushSizeLabel_display.Text = BrushSize + " px";
+
+            Brush = new SolidBrush(Color.Black);
+            Pen = new Pen(Brush, Brushsize);
 
             //Update Visualisation Sample
-            sizeSample.DrawLines(p, new Point[] { 
-                new Point(this.sizePanel.Location.X, this.sizePanel.Location.Y), 
-                new Point(this.sizePanel.Location.X + this.sizePanel.Size.Width, this.sizePanel.Location.Y + this.sizePanel.Size.Height) 
+            SizeSample.DrawLines(Pen, new Point[] { 
+                    //from
+                new Point(sizeSamplePanel.Location.X, sizeSamplePanel.Location.Y), 
+                    // to
+                new Point(sizeSamplePanel.Location.X + sizeSamplePanel.Size.Width, sizeSamplePanel.Location.Y + sizeSamplePanel.Size.Height) 
             }); 
         }
 
-
-
         private void showCoordinates(MouseEventArgs e)
         {
-            label1.Text = "x:" + e.X + "   y:" + e.Y;
+            PointerCoordsLabel_display.Text = "x : " + e.X + ";   y : " + e.Y;
         }
+
         /*
         protected override void OnPaint(PaintEventArgs e)
         {
